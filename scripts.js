e00a45d4ee480055cb4cc3d60eceab8333a398a0
Juliana Braga <jujbraga@gmail.com>
2026-02-05 16:53:13 +0100

    function to start game and shuffle countries

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ const imgPath = "assets/flags/";
 // Quiz varaibles
 let level = 1; // Game level
 let flagsData = []; // List with all flags
+let shuffledData = []; // Countries list shuffled and sorted by level
+let availableFlags = []; // Flags left to show in the current level
+let rightAnswers = []; // List of the right answers
+let wrongAnswers = []; // List of the wrong answers
 
 // Get the countries in JSON file and initiate game
 async function getFlags(file) {
@@ -13,14 +17,43 @@ async function getFlags(file) {
 
     // Assign the JSON result to variable
     flagsData = data.levels;
+
+    // Start the game
+    initGame();
   } catch (error) {
     console.error("Failed to load flags:", error);
   }
 }
 
+function initGame() {
+  const easyMode = flagsData.find((l) => l.difficulty === "easy").data;
+  const mediumMode = flagsData.find((l) => l.difficulty === "medium").data;
+  const hardMode = flagsData.find((l) => l.difficulty === "hard").data;
+
+  // Get the data and shuffle by level
+  switch (level) {
+    case 3:
+      shuffledData = shuffle(hardMode);
+      break;
+    case 2:
+      shuffledData = shuffle(mediumMode);
+      break;
+    default:
+      shuffledData = shuffle(easyMode);
+      break;
+  }
+}
+
+// Shuffle items in the array
+function shuffle(array) {
+  const shuffled = array.sort(() => Math.random() - 0.5);
+
+  return shuffled;
+}
+
 getFlags("eu-flags.json");
 
-// 1. Get data from json
+// 1. Get data from json - OK
 // 2. Set level by click
 // 3. Separate data by level
 // 4. Build the card
